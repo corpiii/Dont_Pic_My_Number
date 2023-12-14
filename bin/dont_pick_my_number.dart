5f1abd379b7a1bfcbf347e54d5929957dde8c2ec
sangwoo <kajer102@gmail.com>
2023-12-14 22:03:04 +0900

    2번까지했는데 출력은 못했습니다.

--- bin/dont_pick_my_number.dart
+++ bin/dont_pick_my_number.dart
@@ -1,5 +1,6 @@
-import 'package:dont_pick_my_number/dont_pick_my_number.dart' as dont_pick_my_number;
+import 'package:dont_pick_my_number/dont_pick_my_number.dart'
+    as dont_pick_my_number;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dont_pick_my_number.calculate()}!');
+  print('Hello world');
 }
