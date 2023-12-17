c091db5adbb3c1bfc00633b8f4a64a518880fc0a
Kim.Yushin <offyushin@naver.com>
2023-12-17 20:05:14 +0900

    내 번호 뽑지마 3단계 중복해결못함

--- lib/random.dart
+++ lib/random.dart
@@ -1,10 +1,34 @@
 import 'dart:math';
+import 'package:collection/collection.dart';
 
 List<int> randomNumber() {
   List<int> randomNumbers = [];
-  List<int> noNumbers =[1, 5, 10, 14, 19, 25];
-  Random random = Random();
+  List<int> noNumbers = [1, 5, 10, 14, 19, 25];
+  List<int> choiceNumbers = [];
+  while (randomNumbers.length < 5) {
+    int number = Random().nextInt(39) + 1;
+    if (!randomNumbers.contains(number) &&
+        !noNumbers.contains(number) &&
+        !choiceNumbers.contains(number)) {
+      randomNumbers.add(number);
+    }
+    randomNumbers.sort();
+  }
+  choiceNumbers.addAll(randomNumbers);
   return randomNumbers;
+  // 왜 retrun 뒤에 sort는 안되는 것인가?
+  // randomNumbers 는 List라서 가능해야 하는거 아닌가? 왜 retrun전에 randomNumbers.sort는 가능한가?
+  // sort의 타입이 void라서 안되는 것인가..?
 }
+
+roundNumber(int round) {
+  for (int i = 1; i <= round; i++) {
+    String numberPrint =
+        randomNumber().map((numberPrint) => '$numberPrint번').join(', ');
+    print('$i회차에서 뽑힌 번호들은 $numberPrint 입니다');
+  }
+}
+
 void main() {
+  roundNumber(5);
 }
